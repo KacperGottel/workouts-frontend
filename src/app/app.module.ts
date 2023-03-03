50ed0a99ad9a130ec3410485b695964ec18b1339
Kacper Gottel <kacper.gottel@appwise.pl>
2023-03-03 14:16:36 +0100

    header done, home page in progres...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {RouterModule, Routes} from "@angular/router";
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/component/header.component';
+import { HomeComponent } from './home/component/home/home.component';
 
 
 const routes: Routes = [
   {
-
+    path: "home", component: HomeComponent, canActivate : [],
   }];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
