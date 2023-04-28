32add23a967c3f5721c30a813c3e10ed4fc71eb5
Kacper Gottel <kacper.gottel@appwise.pl>
2023-04-28 17:07:55 +0200

    css button active fixed

--- src/app/main/component/workout/exercise-details-modal/exercise-details-modal.component.ts
+++ src/app/main/component/workout/exercise-details-modal/exercise-details-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { AfterViewInit, Component, Input } from '@angular/core'
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
 import { Exercise } from '../../../../model/Workout'
 
@@ -7,7 +7,12 @@ import { Exercise } from '../../../../model/Workout'
   templateUrl: './exercise-details-modal.component.html',
   styleUrls: ['./exercise-details-modal.component.css'],
 })
-export class ExerciseDetailsModalComponent {
+export class ExerciseDetailsModalComponent implements AfterViewInit {
   @Input() exercise: Exercise | undefined
+  isActive: boolean = true
   constructor(public modal: NgbActiveModal) {}
+
+  ngAfterViewInit(): void {
+    this.isActive = false
+  }
 }
