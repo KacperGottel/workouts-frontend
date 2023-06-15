c4b87e9e050768931a184864f70d5fb5e5ec2ef8
Kacper Gottel <kacper.gottel@appwise.pl>
2023-06-15 22:01:39 +0200

    modal

--- src/app/main/component/exercise/exercise-add-modal/exercise-add-component-modal.component.ts
+++ src/app/main/component/exercise/exercise-add-modal/exercise-add-component-modal.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-exercise-add',
   templateUrl: './exercise-add-component-modal.component.html',
   styleUrls: ['./exercise-add-component-modal.component.css'],
 })
-export class ExerciseAddComponentModal {
-  constructor(public modal: NgbActiveModal) {}
+export class ExerciseAddComponentModal implements OnInit {
+  exerciseForm: FormGroup
+  constructor(public modal: NgbActiveModal, private fb: FormBuilder) {
+    this.exerciseForm = this.fb.group({
+      name: [{ value: '' }, [Validators.required]],
+      category: [{ value: '' }, [Validators.required]],
+      description: [{ value: '' }, [Validators.required]],
+      videoUrl: [{ value: '' }, [Validators.required]],
+      imgUrl: [{ value: '' }, [Validators.required]],
+      series: [{ value: '' }, [Validators.required]],
+      reps: [{ value: '' }, [Validators.required]],
+    })
+  }
+  ngOnInit(): void {}
+
+  onSubmit() {}
 }
