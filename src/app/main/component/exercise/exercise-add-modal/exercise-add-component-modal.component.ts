8519ef2d38b4a72acaff3cfc9749d6a2fb7f707b
Kacper Gottel <kacper.gottel@appwise.pl>
2023-06-17 22:08:00 +0200

    form and submit object.

--- src/app/main/component/exercise/exercise-add-modal/exercise-add-component-modal.component.ts
+++ src/app/main/component/exercise/exercise-add-modal/exercise-add-component-modal.component.ts
@@ -10,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class ExerciseAddComponentModal implements AfterViewInit {
   exerciseForm: FormGroup
   isReady = false
+  categories = ['PUSH', 'PULL', 'LEGS', 'ACCESSORY']
   constructor(public modal: NgbActiveModal, private fb: FormBuilder) {
     this.exerciseForm = this.fb.group({
       name: ['', [Validators.required]],
@@ -17,12 +18,22 @@ export class ExerciseAddComponentModal implements AfterViewInit {
       description: ['', [Validators.required]],
       videoUrl: [''],
       imgUrl: [''],
-      series: ['', [Validators.required]],
-      reps: ['', [Validators.required]],
+      series: ['', [Validators.required, Validators.min(3), Validators.max(8)]],
+      reps: ['', [Validators.required, Validators.min(2), Validators.max(15)]],
     })
   }
   ngAfterViewInit(): void {
     this.isReady = true
   }
-  onSubmit() {}
+  onSubmit() {
+    if (this.exerciseForm.valid && this.exerciseForm.touched) {
+      const name = this.exerciseForm.get('name')?.value
+      const category = this.exerciseForm.get('category')?.value
+      const description = this.exerciseForm.get('description')?.value
+      const videoUrl = this.exerciseForm.get('videoUrl')?.value
+      const imgUrl = this.exerciseForm.get('imgUrl')?.value
+      const series = this.exerciseForm.get('series')?.value
+      const reps = this.exerciseForm.get('reps')?.value
+    }
+  }
 }
